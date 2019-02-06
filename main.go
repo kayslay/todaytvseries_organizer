06cba4fe40b742b01ca344e78a012a2e6e753220
badewa kayode <badewakayode1@gmail.com>
2019-02-06 01:43:22 +0100

    add pprof

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
 
 	"github.com/kayslay/todaytvseries_organizer/config"
 	"github.com/kayslay/todaytvseries_organizer/helpers"
+	"runtime/pprof"
 )
 
-func main() {
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
+func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 	err := helpers.Start(*config.InitConfig())
 	if err != nil {
 		log.Println(err)
